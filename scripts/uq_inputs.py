013b0a576d86600af8fd2eadf41748f26ec91e00
Bonface-Osoro <bonfaceosoro@gmail.com>
2022-07-18 08:56:56 -0400

    file edits

--- scripts/uq_inputs.py
+++ scripts/uq_inputs.py
@@ -9,7 +9,7 @@ def uq_inputs_generator():
     path = "/Users/osoro/Github/saleos/data/"
     uq_parameters = []
 
-    N = 10 #Number of datapoints to generate
+    altitude_km = np.arange((item["altitude_km"] - 5))
 
     for number in range(N):
         for key, item in parameters.items():
@@ -20,50 +20,50 @@ def uq_inputs_generator():
             mu = item['mu']
             sigma = item['sigma']
             total_area_earth_km_sq = item["total_area_earth_km_sq"]
-            altitude_km = uniform((item["altitude_km"] - 5), (item["altitude_km"] + 5))
-            dl_frequency_Hz = uniform((item["dl_frequency"] - 0.1), (item["dl_frequency"] + 0.1))
+            altitude_km = uniform((item["altitude_km"] - 5), (item["altitude_km"] + 5)) #
+            dl_frequency_Hz = uniform((item["dl_frequency"] - 0.1), (item["dl_frequency"] + 0.1))#
             dl_bandwidth_Hz = item["dl_bandwidth"]
             speed_of_light = item["speed_of_light"]
-            antenna_diameter_m = uniform((item["antenna_diameter"] - 0.2), 
+            antenna_diameter_m = uniform((item["antenna_diameter"] - 0.2), #
                                  (item["antenna_diameter"] + 0.2))
-            antenna_efficiency = uniform((item["antenna_efficiency"] - 0.1), 
+            antenna_efficiency = uniform((item["antenna_efficiency"] - 0.1), #
                                  (item["antenna_efficiency"] + 0.1))
             power_dBw = item["power"]
-            receiver_gain_dB = uniform((item["receiver_gain"] - 5), 
+            receiver_gain_dB = uniform((item["receiver_gain"] - 5), #
                                (item["receiver_gain"] + 5))
-            earth_atmospheric_losses_dB = uniform((item["earth_atmospheric_losses"] - 3), 
+            earth_atmospheric_losses_dB = uniform((item["earth_atmospheric_losses"] - 3), #
                                           (item["earth_atmospheric_losses"] + 3))
-            all_other_losses_dB = uniform((item["all_other_losses"] - 0.2), 
+            all_other_losses_dB = uniform((item["all_other_losses"] - 0.2), #
                                   (item["all_other_losses"] + 0.2))
             number_of_channels = item["number_of_channels"]
             polarization = item["polarization"]
-            monthly_traffic = uniform((item["monthly_traffic_GB"]), 
+            monthly_traffic = uniform((item["monthly_traffic_GB"]), #
                               (item["monthly_traffic_GB"] + 10))
             traffic_percent = item["percent_of_traffic"]
-            adoption_rate = uniform(item["adoption_rate"], (item["adoption_rate"] + 0.99))
+            adoption_rate = uniform(item["adoption_rate"], (item["adoption_rate"] + 0.99))#
             fuel_mass_kg = item["fuel_mass"]
             fuel_mass_1_kg = item["fuel_mass_1"]
             fuel_mass_2_kg = item["fuel_mass_2"]
             fuel_mass_3_kg = item["fuel_mass_3"]
-            satellite_launch_cost = uniform((item["satellite_launch_cost"] - 10000000), 
+            satellite_launch_cost = uniform((item["satellite_launch_cost"] - 10000000), #
                                     (item["satellite_launch_cost"] + 10000000))
-            ground_station_cost = uniform((item["ground_station_cost"] - 5000000), 
+            ground_station_cost = uniform((item["ground_station_cost"] - 5000000), #
                                   (item["ground_station_cost"] + 5000000))
-            spectrum_cost = uniform((item["spectrum_cost"] - 20000000), 
+            spectrum_cost = uniform((item["spectrum_cost"] - 20000000), #
                             (item["spectrum_cost"] + 20000000))
-            regulation_fees = uniform((item["regulation_fees"] - 2000000), 
+            regulation_fees = uniform((item["regulation_fees"] - 2000000), #
                               (item["regulation_fees"] + 2000000))
-            digital_infrastructure_cost = uniform((item["digital_infrastructure_cost"] - 1000000), 
+            digital_infrastructure_cost = uniform((item["digital_infrastructure_cost"] - 1000000), #
                                           (item["digital_infrastructure_cost"] + 1000000))
-            ground_station_energy = uniform((item["ground_station_energy"] - 1000000), 
+            ground_station_energy = uniform((item["ground_station_energy"] - 1000000), #
                                     (item["ground_station_energy"] + 1000000))
-            subscriber_acquisition = uniform((item["subscriber_acquisition"] - 10000000), 
+            subscriber_acquisition = uniform((item["subscriber_acquisition"] - 10000000), #
                                      (item["subscriber_acquisition"] + 10000000))
-            staff_costs = uniform((item["staff_costs"] - 10000000), 
+            staff_costs = uniform((item["staff_costs"] - 10000000), #
                           (item["staff_costs"] + 10000000))
-            research_development = uniform((item["research_development"] - 1000000), 
+            research_development = uniform((item["research_development"] - 1000000), #
                                    (item["research_development"] + 1000000))
-            maintenance = uniform((item["maintenance"] - 1000000), 
+            maintenance = uniform((item["maintenance"] - 1000000), #
                           (item["maintenance"] + 1000000))
             discount_rate = item["discount_rate"]
             assessment_period_year = item["assessment_period"]
