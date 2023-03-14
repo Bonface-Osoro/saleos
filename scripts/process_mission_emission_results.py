84c69303b057b11cf2c43e65d01501061fbd895f
Bonface-Osoro <bonfaceosoro@gmail.com>
2023-03-14 18:32:49 -0400

    emission plots

--- scripts/process_mission_emission_results.py
+++ scripts/process_mission_emission_results.py
@@ -2,7 +2,8 @@ from __future__ import division
 import configparser
 import os
 import math
-import timeit
+import time
+from tqdm import tqdm
 from numpy import savez_compressed
 import pandas as pd
 
@@ -10,40 +11,30 @@ import saleos.sim as sl
 from inputs import lut
 pd.options.mode.chained_assignment = None #Suppress pandas outdate errors.
 
+start = time.time()
 data_path = '/Users/osoro/Github/saleos/results/'
 results_path = '/Users/osoro/Github/saleos/results/'
 
-def process_mission_number(data_path, results_path):
+start = time.time() 
+def process_mission_total(data_path, results_path):
     
     df = pd.read_csv(data_path + "uq_results.csv", index_col=False)
     
     #Select the columns to use.
+    df = df[["constellation", "constellation_capacity", 
+        "capacity_scenario", "capex_costs", "capex_scenario", 
+        "subscribers_low", "subscribers_baseline", "subscribers_high",
+        "total_opex", "total_cost_ownership", "opex_scenario", 
+        "total_global_warming_em", "total_ozone_depletion_em",
+        "total_mineral_depletion", 
+        "total_freshwater_toxicity", "total_human_toxicity", 
+        "total_water_depletion"]]
 
-    df = df[["constellation", "constellation_capacity", "capacity_scenario", "subscribers_low", \
-        "subscribers_baseline", "subscribers_high", "total_emissions_t", "emission_per_capacity", \
-        "emission_per_sqkm", "emission_for_every_cost", "capex_costs", "total_opex", "opex_scenario",
-        "total_cost_ownership", "capex_scenario"]]
+    # Create future columns to use
+    df[["mission_number", "total_emissions"]] = ""
 
-    df = pd.melt(df, id_vars=["constellation", "constellation_capacity", "capacity_scenario", \
-        "total_emissions_t", "emission_per_capacity", "emission_per_sqkm", "emission_for_every_cost",
-        "capex_costs", "total_opex", "opex_scenario", "total_cost_ownership", "capex_scenario"],
-         value_vars=["subscribers_low", "subscribers_baseline", "subscribers_high"])
-
-    df.columns = ["constellation", "constellation_capacity", "capacity_scenario", "total_emissions_t", \
-                "emission_per_capacity", "emission_per_sqkm", "emission_for_every_cost", "capex_costs","total_opex", \
-                "opex_scenario", "total_cost_ownership", "capex_scenario", "subscriber_scenario", "subscribers"]
-   
-    
-    #Create new columns to store the results.
-
-    df[["mission_number", "capacity_per_user", "mission_total_emissions", "mission_emission_per_capacity", 
-    "mission_emission_per_sqkm", "mission_emission_for_every_cost", "emission_per_subscriber",
-    "capex_per_user","opex_per_user", "tco_per_user", "capex_per_capacity", "opex_per_capacity", 
-    "tco_per_capacity"]] = " "
-    
-    # Generate mission number for each constellation.
-
-    for i in df.index:
+    # Process satellite missions       
+    for i in tqdm(df.index, desc = "Processing satellite missions"):
         if df["constellation"].loc[i] == "Starlink":
             df["mission_number"].loc[i]=i
             if df["mission_number"].loc[i]<74:
@@ -51,56 +42,87 @@ def process_mission_number(data_path, results_path):
             else:
                 df["mission_number"].loc[i]=74
         elif df["constellation"].loc[i] == "OneWeb":
-            df["mission_number"].loc[i]=i-(2186)
+            df["mission_number"].loc[i]=i-(6560)
             if df["mission_number"].loc[i]<20:
-                df["mission_number"].loc[i]=i-(2186)
+                df["mission_number"].loc[i]=i-(6560)
             else:
                 df["mission_number"].loc[i]=20
         elif df["constellation"].loc[i] == "Kuiper":
-            df["mission_number"].loc[i]=i-(4373)
+            df["mission_number"].loc[i]=i-(13121)
             if df["mission_number"].loc[i]<54:
-                df["mission_number"].loc[i]=i-(4373)
+                df["mission_number"].loc[i]=i-(13121)
             else:
                 df["mission_number"].loc[i]=54
         else:
             df["mission_number"].loc[i]= 0
-    # Rename subscriber scenario
+    print("Finished processing satellite missions")
 
-    for i in range(len(df)):
-        if df["subscriber_scenario"].loc[i] == "subscribers_low":
-            df["subscriber_scenario"].loc[i] = "Low"
-        elif df["subscriber_scenario"].loc[i] == "subscribers_baseline":
-            df["subscriber_scenario"].loc[i] = "Baseline"
-        else:
-            df["subscriber_scenario"].loc[i] = "High"
+    # Classify subscribers by melting the dataframe into long format
+    df = pd.melt(df, id_vars = ["constellation", "constellation_capacity", 
+        "capacity_scenario", "total_opex", "capex_costs", "capex_scenario",
+        "opex_scenario", "total_cost_ownership", "mission_number", 
+        "total_global_warming_em", "total_ozone_depletion_em", 
+        "total_mineral_depletion", "total_freshwater_toxicity", 
+        "total_human_toxicity", "total_water_depletion", "total_emissions"], 
+        value_vars = ["subscribers_low", "subscribers_baseline", 
+        "subscribers_high",], var_name = "subscriber_scenario", 
+        value_name = "subscribers")
 
-    # Calculate the mission emission results for each constellation.
+    # Classify total emissions by impact category
+    df = pd.melt(df, id_vars = ["constellation", "constellation_capacity", 
+        "capacity_scenario", "total_opex", "capex_costs", "capex_scenario",
+        "opex_scenario", "total_cost_ownership", "mission_number", 
+        "subscriber_scenario", "subscribers", "total_emissions"], 
+        value_vars = ["total_global_warming_em", "total_ozone_depletion_em", 
+        "total_mineral_depletion", "total_freshwater_toxicity", 
+        "total_human_toxicity", "total_water_depletion"], var_name = 
+        "impact_category", value_name = "emission_totals")
 
-    for i in range(len(df)):
-        df["mission_total_emissions"].loc[i] = df["total_emissions_t"].loc[i] \
-                                               * df["mission_number"].loc[i]
-        df["mission_emission_per_capacity"].loc[i] = df["emission_per_capacity"].loc[i] \
-                                                     * df["mission_number"].loc[i]
-        df["mission_emission_per_sqkm"].loc[i] =  df["emission_per_sqkm"].loc[i] \
-                                          * df["mission_number"].loc[i]
-        df["mission_emission_for_every_cost"].loc[i] = df["emission_for_every_cost"].loc[i] \
-                                                       * df["mission_number"].loc[i]
-        df["emission_per_subscriber"].loc[i] = df["total_emissions_t"].loc[i] / \
-                                               df["subscribers"].loc[i]
-        df["capacity_per_user"].loc[i] = df["constellation_capacity"].loc[i] / \
-                                         df["subscribers"].loc[i]
+    # Calculate the total emissions
+    for i in tqdm(range(len(df)), desc = "Calculating constellation emission totals".format(i)):
+        df["total_emissions"].loc[i] = df["emission_totals"].loc[i] * df["mission_number"].loc[i]
+    print("Finished calculating constellation emission totals")
+
+    # Select columns to use
+    df = df[['constellation', 'constellation_capacity', 'capacity_scenario',
+        'total_opex', 'capex_costs', 'capex_scenario', 'opex_scenario',
+        'total_cost_ownership', 'mission_number', 'subscriber_scenario', 
+        'subscribers', 'impact_category', 'total_emissions']]
+
+    #Create columns to store new data
+    df[["capacity_per_user", "emission_per_capacity", "per_cost_emission", 
+        "per_subscriber_emission", "capex_per_user", "opex_per_user", 
+        "tco_per_user", "capex_per_capacity", "opex_per_capacity", 
+        "tco_per_capacity"]] = ""
+
+    # Calculate total metrics
+    for i in tqdm(range(len(df)), desc = "Processing constellation aggregate results".format(i)):
+        df["emission_per_capacity"].loc[i] = df["total_emissions"].loc[i] / df["constellation_capacity"].loc[i]
+        
+        df["per_cost_emission"].loc[i] = df["total_emissions"].loc[i] / df["total_cost_ownership"].loc[i]
+                                                    
+        df["per_subscriber_emission"].loc[i] = df["total_emissions"].loc[i] / df["subscribers"].loc[i]
+        
+        df["capacity_per_user"].loc[i] = df["constellation_capacity"].loc[i] / df["subscribers"].loc[i]
+        
         df["capex_per_user"].loc[i] = df["capex_costs"].loc[i] / df["subscribers"].loc[i] 
-        df["opex_per_user"].loc[i] = df["total_opex"].loc[i] / df["subscribers"].loc[i]   
+        
+        df["opex_per_user"].loc[i] = df["total_opex"].loc[i] / df["subscribers"].loc[i] 
+        
         df["tco_per_user"].loc[i] = df["total_cost_ownership"].loc[i] / df["subscribers"].loc[i]
+        
         df["capex_per_capacity"].loc[i] = df["capex_costs"].loc[i] / df["constellation_capacity"].loc[i]
+        
         df["opex_per_capacity"].loc[i] = df["total_opex"].loc[i] / df["constellation_capacity"].loc[i]
+        
         df["tco_per_capacity"].loc[i] = df["total_cost_ownership"].loc[i] / df["constellation_capacity"].loc[i]
-
-
+         
     store_results = df.to_csv(results_path + "mission_emission_results.csv")
     results_path2 = '/Users/osoro/Github/saleos/vis/'
     store_results = df.to_csv(results_path2 + "mission_emission_results.csv")
 
     return store_results
 
-process_mission_number(data_path, results_path)
+process_mission_total(data_path, results_path)
+executionTime = (time.time() - start)
+print('Execution time in minutes: ' + str(round(executionTime/60, 2)))  
